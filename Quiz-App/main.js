ebcc7da5c8770e28a9743f7b123d3777368c7908
Kamal-kishor1 <kamal2003pandey123@gmail.com>
2024-08-09 22:10:09 +0530

    complication in scroll quiz div

---
+++ Quiz-App/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("button").addEventListener("click", () => {
+    let quizStart = document.querySelector(".quiz");
+    quizStart.classList.remove("start");
+});
+
+const leftScroll = document.querySelector("#leftdiv");
+const rightScroll = document.querySelector("#rightdiv");
+
