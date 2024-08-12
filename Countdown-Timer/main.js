ea358bb6ee8e039ee8cc8e8128c37d85eb558ade
Kamal-kishor1 <kamal2003pandey123@gmail.com>
2024-08-12 15:00:18 +0530

    completed this project but need to improve this project

--- Countdown-Timer/main.js
+++ Countdown-Timer/main.js
@@ -1,19 +1,22 @@
-let startBtn = document.querySelector("#btn1");
-const stopBtn = document.getElementById("btn2");
-const clearBtn = document.getElementById("btn3");
-let clockDay = document.querySelectorAll("#clock-day");
-let clockMin = document.querySelectorAll("#clock-min");
-
+const startBtn = document.querySelector("#start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+const clockDay = document.querySelectorAll("#clock-day");
+const clockMin = document.querySelectorAll("#clock-min");
+let interval = null;
 
 const countDown = () => {
-    let startTime = new Date("August 12, 2024").getTime();
-    setInterval(() => {
+
+    let startTime = new Date("August 12, 2024 15:00:00").getTime();
+    interval = setInterval(() => {
         let currTime = new Date().getTime();
         let remainTime = startTime - currTime;
 
         let day = Math.floor(remainTime / (24 * 60 * 60 * 1000));
         let hr = Math.floor((remainTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let min = Math.floor((remainTime % (1000 * 60 * 60)) / (1000 * 60));
         let sec = Math.floor((remainTime % (1000 * 60)) / (1000));
+
 
         const time = {
             d: day,
@@ -39,45 +42,59 @@ const countDown = () => {
 
         console.log(`${time.d0}${time.d1} : ${time.h0}${time.h1} : ${time.m0}${time.m1} : ${time.s0}${time.s1}`);
 
-        clockDay.forEach((evt) => {
-            evt.firstElementChild.innerText = time.d0;
-            evt.lastElementChild.innerText = time.d1;
-            evt.nextElementSibling.firstElementChild.innerText = time.h0;
-            evt.nextElementSibling.children[1].innerText = time.h1;
-        })
 
-        clockMin.forEach((evt) => {
-            evt.firstElementChild.innerText = time.m0;
-            evt.children[1].innerText = time.m1;
-            evt.nextElementSibling.firstElementChild.innerText = time.s0;
-            evt.nextElementSibling.lastElementChild.innerText = time.s1;
-        })
+        if (remainTime > 0) {
+            clockDay.forEach((evt) => {
+                evt.firstElementChild.innerText = time.d0;
+                evt.lastElementChild.innerText = time.d1;
+                evt.nextElementSibling.firstElementChild.innerText = time.h0;
+                evt.nextElementSibling.children[1].innerText = time.h1;
+            })
+
+            clockMin.forEach((evt) => {
+                evt.firstElementChild.innerText = time.m0;
+                evt.children[1].innerText = time.m1;
+                evt.nextElementSibling.firstElementChild.innerText = time.s0;
+                evt.nextElementSibling.lastElementChild.innerText = time.s1;
+            })
+        }
+        else {
+            clearInterval(interval);
+            console.log("time is cancel");
+        }
+
 
     }, 1000);
 };
 
 
 
-
-
-
-
-
-
-
-
-
 startBtn.addEventListener("click", () => {
-    // countDown();
+    countDown();
 });
 
 
 stopBtn.addEventListener("click", () => {
-    console.log("stop button clicked");
+    clearInterval(interval);
+    console.log("this btn is not fuctional now")
+    clockDay.forEach((evt) => {
+        evt.firstElementChild.innerText = "9";
+        evt.lastElementChild.innerText = "9";
+        evt.nextElementSibling.firstElementChild.innerText = "9";
+        evt.nextElementSibling.children[1].innerText = "9";
+    })
+
+    clockMin.forEach((evt) => {
+        evt.firstElementChild.innerText = "9";
+        evt.children[1].innerText = "9";
+        evt.nextElementSibling.firstElementChild.innerText = "9";
+        evt.nextElementSibling.lastElementChild.innerText = "9";
+    })
 });
 
 
-clearBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", () => {
+    clearInterval(interval);
     clockDay.forEach((evt) => {
         evt.firstElementChild.innerText = "0";
         evt.lastElementChild.innerText = "0";
