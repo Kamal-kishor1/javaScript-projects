78dbceb82a5013865a43519f66401503703e700b
Kamal-kishor1 <kamal2003pandey123@gmail.com>
2024-08-17 22:47:45 +0530

    completed the project and solve the fetching bug with another method

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,36 +1,42 @@
-const apiKey ="4d87f67e851be11e2130f708554e28c4";
-const apiUrl ="https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "4d87f67e851be11e2130f708554e28c4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 
-let btn = document.querySelector("form .btn");
+let btn = document.querySelector(".btn");
 let msg1 = document.querySelector(".location");
 let msg2 = document.querySelector(".temperature");
 let msg3 = document.querySelector(".descrip");
-let searchBox = document.querySelector("form #data");
+let searchBox = document.querySelector("#data");
 
-btn.addEventListener("click",()=>{
+btn.addEventListener("click", () => {
     let inputVal = searchBox.value;
-    if(inputVal != ""){
-        console.log("here is search value is ",inputVal);
-    }
-    if(inputVal){
-        fetchWeather(inputVal);
+
+    // if (inputVal != "") {
+    //     console.log("here is search value is ", inputVal);
+    // }
+    if (inputVal) {
+        wetHer(inputVal);
     }
 });
 
-const fetchWeather = async (inputVal)=>{
-    try{
-    const url =`${apiUrl}?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-    let response = await fetch(url);
-    let data = response.json();
+const wetHer = (inputVal) => {
+    try {
+        let url = `${apiUrl}?q=${inputVal}&appid=${apiKey}&units=metric`;
+        fetch(url).then((response) => response.json()).then((data) => {
+            // console.log(data);
+            msg1.innerText = data.name;
+            msg2.innerText = `${Math.round(data.main.temp)}°C`;
+            msg3.innerText = data.weather[0].description;
+        })
+    }
+    catch (error) {
+        console.error("Error fetching the weather data:", error);
+    }
+}
 
-    console.log(data);
-     msg1.innerText = data.name;
-     msg2.innerText = `${Math.round(data.main.temp)}°C`;
-     msg3.innerText = data.weather[0].description;
-    }
-    catch (error){
-        console.error("Error fetching the weather data:",error);
-    }
-};
+
+
+
+
+
