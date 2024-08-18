9da9e00416ce210d0e4ed1d8c855cdd2d19c01f5
Kamal-kishor1 <kamal2003pandey123@gmail.com>
2024-08-19 01:01:05 +0530

    only logic application is left

--- Expense_Tracker/main.js
+++ Expense_Tracker/main.js
@@ -21,14 +21,76 @@ setBudgetBtn.addEventListener("click", () => {
 
 
 checkExpenseBtn.addEventListener("click", () => {
-    if (tAmount.lastElementChild.innerText != "") {
+    let tCost = tAmount.lastElementChild.innerText;
+    if (tCost != "") {
         let productName = inputProduct.value;
         let productVal = inputAmount.value;
-        console.log(productName, productVal);
+        checkAmount(tCost, productName, productVal);
+        // expensesList(productName, productVal);
+        // inputProduct.value = "";
+        // inputAmount.value = "";
     }
     else {
         alert("First set your budget please!");
     }
-})
+});
 
+const expensesList = (productName, productVal) => {
+    const contentFlex = document.querySelector(".content-flex");
 
+    let checkList = document.createElement("div");
+    checkList.className = "flex-checklist";
+    contentFlex.append(checkList);
+
+    let vBar = document.createElement("div");
+    vBar.className = "verticalebar";
+    checkList.append(vBar);
+
+    let pName = document.createElement("p");
+    pName.id = "pname";
+    checkList.append(pName);
+    pName.innerText = productName;
+
+    let pCost = document.createElement("p");
+    pCost.id = "pcost";
+    checkList.append(pCost);
+    pCost.innerText = productVal;
+
+    let editBtn = document.createElement("div");
+    editBtn.className = "edit-btn";
+    checkList.append(editBtn);
+
+    let editIcon = document.createElement("i");
+    editIcon.className = "fa fa-edit";
+    editIcon.ariaHidden = "true";
+    editBtn.append(editIcon);
+
+    let deleteBtn = document.createElement("div");
+    deleteBtn.className = "delete-btn";
+    checkList.append(deleteBtn);
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa fa-trash";
+    deleteIcon.ariaHidden = "true";
+    deleteBtn.append(deleteIcon);
+
+    // console.log(checkList);
+};
+
+const checkAmount = (tCost, productName, productVal) => {
+    let expenses = 0;
+    expenses += productVal;
+    if (productVal === "") {
+        alert("Please also enter the cost!");
+    } else {
+        let balance = tCost - expenses;
+        let balHead = document.querySelector(".head3");
+        balHead.lastElementChild.innerText = balance;
+        if (balance >= tCost) {
+            alert("You have reached your limit!");
+            return balance;
+        }
+        expensesList(productName, productVal);
+
+    }
+};
